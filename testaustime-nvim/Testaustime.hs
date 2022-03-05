8ef431744dd79ae40eeb78aba91f6c3a75b0a158
Luukas Pörtfors <lajp@lajp.fi>
2022-03-05 19:41:59 +0200

    Minor codebase cleanup

--- testaustime-nvim/Testaustime.hs
+++ testaustime-nvim/Testaustime.hs
@@ -6,10 +6,10 @@ module Testaustime (testaustimeHeartBeat, initTestaustimeEnv) where
 import GHC.Generics
 import Neovim
 import Neovim.API.String
-import UnliftIO.STM  (TVar, atomically, readTVar, writeTVar, newTVarIO)
+import UnliftIO.STM  (TVar, atomically, writeTVar, newTVarIO, readTVarIO)
 import Control.Monad (when)
-import Data.Time.Clock.POSIX
-import Data.Time
+import Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
+import Data.Time (UTCTime, nominalDiffTimeToSeconds, getCurrentTime)
 import Data.Aeson (encode, object, (.=), ToJSON)
 import Data.ByteString.Char8 (unpack, pack)
 import qualified Data.ByteString.Lazy.Char8 as L8
@@ -17,7 +17,12 @@ import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 import Network.HTTP.Types.Status (statusCode)
 
-data HeartBeat = HeartBeat { project_name :: Maybe String, language :: Maybe String, editor_name :: Maybe String, hostname :: Maybe String} deriving Generic
+data HeartBeat = HeartBeat {
+    project_name :: Maybe String,
+    language :: Maybe String,
+    editor_name :: Maybe String,
+    hostname :: Maybe String }
+    deriving Generic
 
 instance ToJSON HeartBeat
 
@@ -28,9 +33,8 @@ toString :: Neovim.Object -> String
 toString (ObjectString x) = unpack x
 
 toMaybe :: String -> Maybe String
-toMaybe x
-    | x == "" = Nothing
-    | otherwise = Just x
+toMaybe "" = Nothing
+toMaybe x = Just x
 
 secsSinceEpoch :: UTCTime -> Int
 secsSinceEpoch = floor . nominalDiffTimeToSeconds . utcTimeToPOSIXSeconds
@@ -38,12 +42,11 @@ secsSinceEpoch = floor . nominalDiffTimeToSeconds . utcTimeToPOSIXSeconds
 testaustimeHeartBeat :: Neovim (TVar Int) ()
 testaustimeHeartBeat = do
     lastUpdatedTVar <- ask
-    lastUpdated <- atomically $ do readTVar lastUpdatedTVar
+    lastUpdated <- readTVarIO lastUpdatedTVar
     curTime <- liftIO getCurrentTime
     let curTimeSec = secsSinceEpoch curTime
     when ((curTimeSec-lastUpdated) > 20) $ do
-        atomically $ do
-            writeTVar lastUpdatedTVar curTimeSec
+        atomically $ writeTVar lastUpdatedTVar curTimeSec
         hb <- getHeartBeatData
         url <- getUrl
         token <- getAuthorizationToken
@@ -58,10 +61,10 @@ getHeartBeatData = do
         lang <- toString <$> buffer_get_option bf "filetype"
         pName <- reverse . takeWhile (/='/') . reverse . toString <$> nvim_call_function "getcwd" []
         hName <- toString <$> nvim_call_function "hostname" []
-        let eName = "NeoVim"
+        let eName = "NeoVim" -- FIXME: Maybe not hard-coded?
         return (HeartBeat (toMaybe pName) (toMaybe lang) (toMaybe eName) (toMaybe hName))
 
-    else do return (HeartBeat Nothing Nothing Nothing Nothing)
+    else return (HeartBeat Nothing Nothing Nothing Nothing)
 
 getAuthorizationToken :: Neovim env String
 getAuthorizationToken = toString <$> nvim_get_var "testaustime_token"
@@ -73,13 +76,13 @@ sendHeartBeat :: HeartBeat -> String -> String -> IO (Response L8.ByteString)
 sendHeartBeat hb url token = do
     manager <- newManager tlsManagerSettings
     initRequest <- parseRequest url
-    let request = initRequest { method = "POST"
-        , requestBody = RequestBodyLBS $ encode hb
-        , requestHeaders =
-            [ ("Content-Type", "application/json; charset=utf-8")
-            , ("Authorization", pack ("Bearer " ++ token))
+    let request = initRequest {
+        method = "POST",
+        requestBody = RequestBodyLBS $ encode hb,
+        requestHeaders = [
+            ("Content-Type", "application/json; charset=utf-8"),
+            ("Authorization", pack ("Bearer " ++ token))
             ]
         }
 
-    response <- httpLbs request manager
-    return response
+    httpLbs request manager
